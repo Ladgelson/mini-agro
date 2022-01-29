b38c111cfd93653d21af38c13aa3a5356b8bd349
Ladgelson <micaiasladgelsondasilva@gmail.com>
2022-01-29 13:22:58 -0300

    feat: test main

--- backend/src/test/java/com/mini/agro/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/mini/agro/backend/BackendApplicationTests.java
@@ -1,5 +1,7 @@
 package com.mini.agro.backend;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BackendApplicationTests {
 
 	@Test
-	void contextLoads() {
+	@DisplayName("Test function main")
+	void main() {
+		Assertions.assertNotNull(BackendApplication.class);
+		BackendApplication.main(new String[] {});
 	}
 
 }
