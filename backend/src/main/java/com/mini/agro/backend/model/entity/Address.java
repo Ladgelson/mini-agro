8c6a264e0d8fa111b9d57b573b096195cd3416cb
Ladgelson <micaiasladgelsondasilva@gmail.com>
2022-01-29 15:11:35 -0300

    feat: add validation on Address entity

--- backend/src/main/java/com/mini/agro/backend/model/entity/Address.java
+++ backend/src/main/java/com/mini/agro/backend/model/entity/Address.java
@@ -5,22 +5,30 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Address {
 
+    @NotNull
     private String city;
 
+    @NotNull
     private String country;
 
+    @NotNull
     private String province;
 
+    @NotNull
     private String street;
 
+    @NotNull
     private String zipCode;
 
+    @NotNull
     private int number;
 
 }
