5302389b41d7c782a8104f15e7ccd4aeb15b86f3
Ladgelson <micaiasladgelsondasilva@gmail.com>
2022-01-29 14:02:25 -0300

    chore: remove abstract of AbstractModel

--- backend/src/main/java/com/mini/agro/backend/model/entity/base/AbstractModel.java
+++ backend/src/main/java/com/mini/agro/backend/model/entity/base/AbstractModel.java
@@ -1,12 +1,14 @@
 package com.mini.agro.backend.model.entity.base;
 
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.Instant;
 
-public abstract class AbstractModel {
+@Data
+public class AbstractModel {
 
     @Id
     private String id;
