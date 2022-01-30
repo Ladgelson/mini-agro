d134942002ced1d4c3be94b136d15a386c4f5f37
Ladgelson <micaiasladgelsondasilva@gmail.com>
2022-01-30 07:59:01 -0300

    feat: add find farm controller

---
+++ backend/src/main/java/com/mini/agro/backend/controller/FindFarmController.java
@@ -0,0 +1,20 @@
+package com.mini.agro.backend.controller;
+
+import com.mini.agro.backend.model.dto.FarmDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springdoc.api.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "Farm", description = "Resource to manage data about Farms")
+@RequestMapping("/v1/farms")
+public interface FindFarmController {
+    @Operation(description = "Find a list of Farm")
+    @GetMapping
+    ResponseEntity<Page<FarmDto>> process(@ParameterObject @PageableDefault(size = 20, sort = "name") Pageable pageable);
+}
---
+++ backend/src/main/java/com/mini/agro/backend/controller/impl/FindFarmControllerImpl.java
@@ -0,0 +1,37 @@
+package com.mini.agro.backend.controller.impl;
+
+import com.mini.agro.backend.controller.FindFarmController;
+import com.mini.agro.backend.mapper.FarmMapper;
+import com.mini.agro.backend.model.dto.FarmDto;
+import com.mini.agro.backend.service.FindFarmService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@AllArgsConstructor
+public class FindFarmControllerImpl implements FindFarmController {
+
+    private final FindFarmService findFarmService;
+
+    private final FarmMapper mapper;
+
+    @Override
+    public ResponseEntity<Page<FarmDto>> process(Pageable pageable) {
+        List<FarmDto> farmDtoList = findFarmService
+                .execute(pageable)
+                .stream()
+                .map(mapper::farmToFarmDto)
+                .collect(Collectors.toList());
+        if(farmDtoList.isEmpty())
+            return ResponseEntity.noContent().build();
+        Page<FarmDto> response = new PageImpl<>(farmDtoList, pageable, farmDtoList.size());
+        return ResponseEntity.ok(response);
+    }
+}
