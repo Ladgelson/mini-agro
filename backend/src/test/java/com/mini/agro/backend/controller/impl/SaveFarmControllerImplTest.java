86e4d0efbfa3c4f9922a1decc59ddd7d325e7845
Ladgelson <micaiasladgelsondasilva@gmail.com>
2022-01-31 09:51:42 -0300

    test: create tests

---
+++ backend/src/test/java/com/mini/agro/backend/controller/impl/SaveFarmControllerImplTest.java
@@ -0,0 +1,73 @@
+package com.mini.agro.backend.controller.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mini.agro.backend.mock.model.FarmMock;
+import com.mini.agro.backend.model.entity.Farm;
+import com.mini.agro.backend.service.SaveFarmService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class SaveFarmControllerImplTest {
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private SaveFarmService saveFarmService;
+
+    private Farm farmMock;
+
+    @BeforeEach
+    void setUp() {
+        farmMock = FarmMock.createFarm();
+        Mockito.when(saveFarmService.executeSave(farmMock))
+                .thenReturn(farmMock);
+    }
+
+    @Test
+    @DisplayName("Must save a new Farm")
+    void process() throws Exception {
+        mvc.perform(post("/v1/farms")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(farmMock)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.cnpj").value(farmMock.getCnpj()))
+                .andExpect(jsonPath("$.legalName").value(farmMock.getLegalName()))
+                .andExpect(jsonPath("$.tradeName").value(farmMock.getTradeName()))
+                .andExpect(jsonPath("$.address.city").value(farmMock.getAddress().getCity()))
+                .andExpect(jsonPath("$.address.country").value(farmMock.getAddress().getCountry()))
+                .andExpect(jsonPath("$.address.province").value(farmMock.getAddress().getProvince()))
+                .andExpect(jsonPath("$.address.street").value(farmMock.getAddress().getStreet()))
+                .andExpect(jsonPath("$.address.zipCode").value(farmMock.getAddress().getZipCode()))
+                .andExpect(jsonPath("$.address.number").value(farmMock.getAddress().getNumber()));
+    }
+
+    @Test
+    @DisplayName("Must throw exception when required field is missing")
+    void processWhenRequiredFieldIsMissing() throws Exception {
+        farmMock.setCnpj(null);
+        mvc.perform(post("/v1/farms")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(farmMock)))
+                .andExpect(status().isBadRequest());
+    }
+}
---
+++ backend/src/test/java/com/mini/agro/backend/service/impl/FindPlotByIdServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.mini.agro.backend.service.impl;
+
+import com.mini.agro.backend.mock.model.PlotMock;
+import com.mini.agro.backend.model.entity.Plot;
+import com.mini.agro.backend.repository.PlotRepository;
+import com.mini.agro.backend.service.FindPlotByIdService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FindPlotByIdServiceImplTest {
+
+    @Autowired
+    private FindPlotByIdService findPlotByIdService;
+
+    @MockBean
+    private PlotRepository plotRepository;
+
+    private Plot plotMock;
+
+    @BeforeEach
+    void setUp() {
+        plotMock = PlotMock.createPlot();
+        Mockito.when(plotRepository.findById("1"))
+                .thenReturn(Optional.of(plotMock));
+    }
+
+    @Test
+    @DisplayName("Must find a valid plot on the database")
+    void execute() {
+        Plot plot = findPlotByIdService.execute("1");
+        assertEquals(plotMock.getProductions(), plot.getProductions());
+        assertEquals(plotMock.getArea(), plot.getArea());
+    }
+}
