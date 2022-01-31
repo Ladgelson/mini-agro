710d80f562696b5caf83774ccb3aa9ca41665e05
Ladgelson <micaiasladgelsondasilva@gmail.com>
2022-01-31 09:41:40 -0300

    feat: create controller to calculate productivity by plot

---
+++ backend/src/main/java/com/mini/agro/backend/controller/CalculateProductionByPlotController.java
@@ -0,0 +1,28 @@
+package com.mini.agro.backend.controller;
+
+import com.mini.agro.backend.model.dto.ProductivityDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "Productivity", description = "Calculate the productivity about a domain")
+@RequestMapping("/v1/plots")
+public interface CalculateProductionByPlotController {
+    @Operation(description = "Calculate a productivity")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Productivity calculated by Plot", content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProductivityDto.class)
+                    )}),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)})
+    @GetMapping("/{id}/productivity")
+    ResponseEntity<ProductivityDto> process(@PathVariable String id);
+}
---
+++ backend/src/main/java/com/mini/agro/backend/controller/impl/CalculateProductionByPlotControllerImpl.java
@@ -0,0 +1,20 @@
+package com.mini.agro.backend.controller.impl;
+
+import com.mini.agro.backend.controller.CalculateProductionByPlotController;
+import com.mini.agro.backend.model.dto.ProductivityDto;
+import com.mini.agro.backend.service.CalculateProductionByPlotService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class CalculateProductionByPlotControllerImpl implements CalculateProductionByPlotController {
+
+    private final CalculateProductionByPlotService calculateProductionByPlotService;
+
+    @Override
+    public ResponseEntity<ProductivityDto> process(String id) {
+        return ResponseEntity.ok(calculateProductionByPlotService.calculateProduction(id));
+    }
+}
